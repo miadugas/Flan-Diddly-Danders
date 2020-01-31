1b1168900d90375c4d71a0946f62bc3d344665ae
Mia Dugas <miadugas@outlook.com>
2020-01-30 18:17:58 -0700

    optimized
    changed some functions to methods

--- src/index.js
+++ src/index.js
@@ -1,45 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./App";
-
+//
 import "./styles.css";
 
 class App extends React.Component {
+  joke = null;
 
-joke = null;
+  constructor() {
+    super();
 
-constructor() {
-super();
+    this.onTellJoke = this.onTellJoke.bind(this);
+  }
 
-this.onTellJoke = this.onTellJoke.bind(this);
+  onTellJoke() {
+    fetch("https://icanhazdadjoke.com/", {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.joke = json.joke;
+        console.log("joke", this.joke);
+      });
+  }
 
-}
+  render() {
+    console.log("-----RENDER-----");
 
-onTellJoke () {
-  fetch ("https://icanhazdadjoke.com/", {
-  method:"GET",
-  headers: {
-    Accept: "application/json"
-}
-  })
-.then(response => response.json())
-.then (json => {
-  this.joke = json.joke;
-  console.log("joke", this.joke);
-});
-};
-
-render() {
-
-console.log("-----RENDER-----");
-
-  return (
-    <div>
-    <button onClick={this.onTellJoke}>Tell me a joke</button>;
-<p>{this.joke}</p>
-    </div>
-  );
-}
+    return (
+      <div>
+        <button onClick={this.onTellJoke}>Tell me a joke</button>;
+        <p>{this.joke}</p>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
